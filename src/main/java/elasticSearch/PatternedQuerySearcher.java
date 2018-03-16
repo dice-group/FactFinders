698b7a17bb13a6c820f6620e1a6ebab4bb9e28fb
sHussainAbid <hussainabid99@yahoo.com>
2018-03-16 03:39:36 +0100

    Refactoring

--- src/main/java/elasticSearch/PatternedQuerySearcher.java
+++ src/main/java/elasticSearch/PatternedQuerySearcher.java
@@ -36,7 +36,7 @@ public class PatternedQuerySearcher extends Searcher{
 	private String object;
 	private PatternFinder wordnetPatternGenerator;
 	private String indexLink;
-	
+
 	public PatternedQuerySearcher(){
 		this.indexLink = "/clueweb/articles/_search";
 		this.searchResult = new SearchResult();
@@ -76,129 +76,129 @@ public class PatternedQuerySearcher extends Searcher{
 
 					singleSearch(q1);
 				}
-				
-				searchResult.claim = claim.replace("\t", "_");
-				searchResult.sources = pageTitles;
-				return searchResult;
-			}
-		}
-			catch (Exception e) {
-
-				e.printStackTrace();
-				return null;
-			}
-		}
-
-		/**
-		 * This query function queries the index (either clue-web or Wiki-dump for these experiments)
-		 * The query is elevated to multiple queries by using several predicate patterns.
-		 * These patterns are already saved during pre-processing to make the search faster
-		 * In pre-processing Word-net or BOA pattern generators are used to store patterns as .tsv file.
-		 * @param claim is the fact to be queried/searched on index
-		 * @param propertyWords is a list of pattern words generated after pre-processing
-		 */
-		@Override
-		public SearchResult query(String claim, ArrayList<String> propertyWords) {
-			try {
-				this.pageTitles = new LinkedHashSet<String>();
-				claimParser(claim);
-				normalize();
-
-				for(String predicate : propertyWords) {
-					String q1;
-					if(predicate.equals("No_Pattern")) {
-						q1 = String.format("\"%s %s %s\"", subject, property, object);
-					}
-					else
-						q1 = String.format("\"%s %s %s\"", subject, predicate.trim(), object);
-					if(singleSearch(q1) != null)
-						pageTitles.addAll(singleSearch(q1));
-
-				}
 
 				searchResult.claim = claim.replace("\t", "_");
 				searchResult.sources = pageTitles;
 				return searchResult;
 			}
-			catch (Exception e) {
-
-				e.printStackTrace();
-				return null;
-			}
 		}
+		catch (Exception e) {
 
-		/**
-		 * Vertex is read from the dataset which contains tab separated subject, predicate and object
-		 * THey are saved in the local variables respectively.
-		 * @param claim
-		 */
-		private void claimParser(String claim) {
-			this.parts  = claim.split("\\t");
-			this.subject   = this.parts[0];
-			this.property  = this.parts[1];
-			this.object    = this.parts[2];
-
-		}
-
-		/**
-		 * Normalization of keywords to be queried in search
-		 */
-		private void normalize(){
-			this.subject  = this.subject.replace("&", "and");
-			this.object   = this.object.replace("&", "and");
-			this.property = this.normalizePredicate(property.trim());
-		}
-
-		/**
-		 * Single search implements the singelton search on the index the result is returned to the caller
-		 */
-		private Set<String> singleSearch(String query){
-			try {
-				Set<String> searchList = new LinkedHashSet<String>();
-				HttpEntity entity1 = new NStringEntity(
-						"{\n" +
-								"	\"size\" : 10000,\n" +
-								"    \"query\" : {\n" +
-								"    \"match_phrase\" : {\n"+
-								"	 \"Article\" : {\n" +
-								"	\"query\" : "+query+",\n"+
-								"	\"slop\"  : 100 \n"+
-								"} \n"+
-								"} \n"+
-								"} \n"+
-								"}", ContentType.APPLICATION_JSON);
-				long start = System.nanoTime();					
-				response = restClientobj.performRequest("GET", indexLink ,Collections.singletonMap("pretty", "true"),entity1, consumerFactory);
-				System.out.print("Search on server took " + TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - start) + " seconds \n");
-				BufferedReader in = new BufferedReader(
-						new InputStreamReader(response.getEntity().getContent(), "UTF-8"));		
-				ObjectMapper mapper = new ObjectMapper();
-				JsonNode rootNode = mapper.readValue(in, JsonNode.class);
-				in.close();
-				JsonNode hits = rootNode.get("hits");
-				JsonNode hitCount = hits.get("total");
-				int docCount = Integer.parseInt(hitCount.asText());
-				if (docCount != 0) {
-					if(docCount > 10000) {
-						docCount = 10000;
-					}
-					for (int i = 0; i < docCount; i++) {
-						JsonNode articleURLNode = hits.get("hits").get(i).get("_source").get("URL");
-	
-						String articleURL = getDomainName(articleURLNode.asText());
-	
-						if (!searchList.contains(articleURL)) {
-							searchList.add(articleURL);
-						}
-					} 
-				}
-				
-				return searchList;
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-				return null;
-			}
+			e.printStackTrace();
+			return null;
 		}
 	}
+
+	/**
+	 * This query function queries the index (either clue-web or Wiki-dump for these experiments)
+	 * The query is elevated to multiple queries by using several predicate patterns.
+	 * These patterns are already saved during pre-processing to make the search faster
+	 * In pre-processing Word-net or BOA pattern generators are used to store patterns as .tsv file.
+	 * @param claim is the fact to be queried/searched on index
+	 * @param propertyWords is a list of pattern words generated after pre-processing
+	 */
+	@Override
+	public SearchResult query(String claim, ArrayList<String> propertyWords) {
+		try {
+			this.pageTitles = new LinkedHashSet<String>();
+			claimParser(claim);
+			normalize();
+
+			for(String predicate : propertyWords) {
+				String q1;
+				if(predicate.equals("No_Pattern")) {
+					q1 = String.format("\"%s %s %s\"", subject, property, object);
+				}
+				else
+					q1 = String.format("\"%s %s %s\"", subject, predicate.trim(), object);
+				if(singleSearch(q1) != null)
+					pageTitles.addAll(singleSearch(q1));
+
+			}
+
+			searchResult.claim = claim.replace("\t", "_");
+			searchResult.sources = pageTitles;
+			return searchResult;
+		}
+		catch (Exception e) {
+
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * Vertex is read from the dataset which contains tab separated subject, predicate and object
+	 * THey are saved in the local variables respectively.
+	 * @param claim
+	 */
+	private void claimParser(String claim) {
+		this.parts  = claim.split("\\t");
+		this.subject   = this.parts[0];
+		this.property  = this.parts[1];
+		this.object    = this.parts[2];
+
+	}
+
+	/**
+	 * Normalization of keywords to be queried in search
+	 */
+	private void normalize(){
+		this.subject  = this.subject.replace("&", "and");
+		this.object   = this.object.replace("&", "and");
+		this.property = this.normalizePredicate(property.trim());
+	}
+
+	/**
+	 * Single search implements the singelton search on the index the result is returned to the caller
+	 */
+	private Set<String> singleSearch(String query){
+		try {
+			Set<String> searchList = new LinkedHashSet<String>();
+			HttpEntity entity1 = new NStringEntity(
+					"{\n" +
+							"	\"size\" : 10000,\n" +
+							"    \"query\" : {\n" +
+							"    \"match_phrase\" : {\n"+
+							"	 \"Article\" : {\n" +
+							"	\"query\" : "+query+",\n"+
+							"	\"slop\"  : 100 \n"+
+							"} \n"+
+							"} \n"+
+							"} \n"+
+							"}", ContentType.APPLICATION_JSON);
+			long start = System.nanoTime();					
+			response = restClientobj.performRequest("GET", indexLink ,Collections.singletonMap("pretty", "true"),entity1, consumerFactory);
+			System.out.print("Search on server took " + TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - start) + " seconds \n");
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(response.getEntity().getContent(), "UTF-8"));		
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode rootNode = mapper.readValue(in, JsonNode.class);
+			in.close();
+			JsonNode hits = rootNode.get("hits");
+			JsonNode hitCount = hits.get("total");
+			int docCount = Integer.parseInt(hitCount.asText());
+			if (docCount != 0) {
+				if(docCount > 10000) {
+					docCount = 10000;
+				}
+				for (int i = 0; i < docCount; i++) {
+					JsonNode articleURLNode = hits.get("hits").get(i).get("_source").get("URL");
+
+					String articleURL = getDomainName(articleURLNode.asText());
+
+					if (!searchList.contains(articleURL)) {
+						searchList.add(articleURL);
+					}
+				} 
+			}
+
+			return searchList;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
 
